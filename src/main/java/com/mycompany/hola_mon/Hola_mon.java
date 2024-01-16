7c90701dd6bb12cae6b66aabd0d85a4594585e86
Lord Stoker <tpc00@iesemilidarder.com>
2024-01-16 10:39:06 +0100

    Primera versión

---
+++ src/main/java/com/mycompany/hola_mon/Hola_mon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hola_mon;
+
+/**
+ *
+ * @author Tomas
+ */
+public class Hola_mon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
